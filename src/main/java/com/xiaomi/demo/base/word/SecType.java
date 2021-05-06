d2ef54d7619d4819cfc381a32ecd4d09f48f11ea
liuchiyun <m15879038981@163.com>
2021-05-06 16:14:03 +0800

    day1

---
+++ src/main/java/com/xiaomi/demo/base/word/SecType.java
@@ -0,0 +1,36 @@
+package com.xiaomi.demo.base.word;
+
+/**
+ * @Author：liuchiyun
+ * @Date: 2021/4/30
+ */
+public class SecType {
+    /**
+     *  股票
+     */
+    private String STK;
+    /**
+     *  期权
+     */
+    private String OPT;
+    /**
+     *  期货
+     */
+    private String FUT;
+    /**
+     *  外汇
+     */
+    private String FOREX;
+    /**
+     *  涡轮
+     */
+    private String WAR;
+    /**
+     * 牛熊证
+     */
+    private String IOPT;
+    /**
+     * 数字货币
+     */
+    private String CC;
+}
---
+++ src/main/java/com/xiaomi/demo/base/word/TestDate.java
@@ -0,0 +1,23 @@
+package com.xiaomi.demo.base.word;
+
+import org.junit.Test;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.logging.SimpleFormatter;
+
+/**
+ * @Author：liuchiyun
+ * @Date: 2021/4/30
+ */
+public class TestDate {
+
+    @Test
+    public  void test(){
+        Date date = new Date(1619767477L*1000);
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(dateFormat.format(date));
+    }
+}
