191a0fc513714e4f486808705b3a87d950df9d93
liuchiyun <m15879038981@163.com>
2021-05-10 10:49:31 +0800

    day 4

--- src/main/java/com/xiaomi/demo/net/TestIO.java
+++ src/main/java/com/xiaomi/demo/net/TestIO.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -19,11 +20,8 @@ public class TestIO {
 
     @Test
     public void test() throws FileNotFoundException {
-//        EpollSocketChannel
-
-          List<List<Integer>> l=new ArrayList<>();
-          ArrayList<Integer> list = new ArrayList<>();
-
+//
+       firstMissingPositive(new int[]{-1,1,2,0,4});
     }
 
 
@@ -47,5 +45,41 @@ public class TestIO {
     private boolean compl(List<Integer> p1, List<Integer> p2, List<Integer> p3){
         int x1=p1.get(0)-p2.get(0),y1=p1.get(1)-p2.get(1),x2=p3.get(0)-p2.get(0),y2=p3.get(1)-p2.get(1);
         return  x1*x2+y1*y2>=0?true:false;
+    }
+
+    public int firstMissingPositive(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i<n;){
+            if(nums[i]<0 || nums[i]>=n || nums[i]==i){
+                i++;
+                continue;
+            }
+            if(nums[nums[i]]!=nums[i]){
+                swap(nums,nums[i],i);
+            }
+            else i++;
+        }
+        System.out.println(Arrays.toString(nums));
+        for(int i=1;i<n;i++){
+            if(nums[i]!=i){
+                return  i;
+            }
+        }
+        return  n;
+    }
+
+    private void swap(int nums[],int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+
+    class TreeNode{
+        TreeNode left=null;
+        TreeNode right=null;
+        int val;
+       TreeNode(int val){
+           this.val=val;
+       }
     }
 }
