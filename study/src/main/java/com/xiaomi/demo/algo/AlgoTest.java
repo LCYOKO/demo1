e9780eea4aa089855c4c8b4203daea89f699ddd4
liuchiyun <m15879038981@163.com>
2024-02-02 16:29:12 +0800

    add

--- study/src/main/java/com/xiaomi/demo/algo/AlgoTest.java
+++ study/src/main/java/com/xiaomi/demo/algo/AlgoTest.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -12,24 +13,69 @@ import java.util.List;
  */
 @Slf4j
 public class AlgoTest {
-    List<List<Integer>> ans = new ArrayList<>();
+
     @Test
-    public void combine() {
-        dfs(1, 4, 2, new ArrayList<>());
+    public void testAlgo() {
+        solveSudoku(new char[][]{
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        });
     }
 
-    private void dfs(int cur, int n, int k, List<Integer> list) {
-        if (cur > n) {
+    int[][] cols = new int[9][9];
+    int[][] rows = new int[9][9];
+    int[][] tables = new int[9][9];
+    List<int[]> poss = new ArrayList<>();
+    boolean valid = false;
+
+    public void solveSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int curTableIdx = (i / 3) * 3 + j / 3;
+                    int k = board[i][j] - '1';
+                    cols[j][k] = 1;
+                    rows[i][k] = 1;
+                    tables[curTableIdx][k] = 1;
+                } else {
+                    poss.add(new int[]{i, j});
+                }
+            }
+        }
+        dfs(0, board);
+        for (char c[] : board) {
+            System.out.println(Arrays.toString(c));
+        }
+    }
+
+    private void dfs(int pos, char[][] board) {
+        if (pos == poss.size()) {
+            valid = true;
             return;
         }
-        if (list.size() == k) {
-            ans.add(new ArrayList<>(list));
-            return;
-        }
-        for (int i = cur; i <= n; i++) {
-            list.add(i);
-            dfs(i + 1, n, k, list);
-            list.remove(list.size() - 1);
+        int i = poss.get(pos)[0];
+        int j = poss.get(pos)[1];
+        int curTableIdx = (i / 3) * 3 + j / 3;
+        for (int k = 0; k < 9 && !valid; k++) {
+            if (cols[j][k] != 0 || rows[i][k] != 0 || tables[curTableIdx][k] != 0) {
+                continue;
+            }
+            cols[j][k] = 1;
+            rows[i][k] = 1;
+            tables[curTableIdx][k] = 1;
+            board[i][j] = (char) ('0' + k);
+            dfs(pos + 1, board);
+            cols[j][k] = 0;
+            rows[i][k] = 0;
+            tables[curTableIdx][k] = 0;
+            board[i][j] = '.';
         }
     }
 }
