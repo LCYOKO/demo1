21c55adc82226a25bb46df1f22460ff8779ae864
liuchiyun <m15879038981@163.com>
2024-02-06 17:49:03 +0800

    add

--- study/src/main/java/com/xiaomi/demo/algo/AlgoTest.java
+++ study/src/main/java/com/xiaomi/demo/algo/AlgoTest.java
@@ -3,6 +3,9 @@ package com.xiaomi.demo.algo;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Random;
+
 /**
  * @Author: liuchiyun
  * @Date: 2024/1/24
@@ -12,5 +15,6 @@ public class AlgoTest {
 
     @Test
     public void test() {
+        new HashMap<>().putIfAbsent()
     }
 }
