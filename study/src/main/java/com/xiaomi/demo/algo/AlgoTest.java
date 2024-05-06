54ea44531e8d4cda39877f2b08880a07cde15578
liuchiyun <m15879038981@163.com>
2024-05-06 20:27:43 +0800

    add

--- study/src/main/java/com/xiaomi/demo/algo/AlgoTest.java
+++ study/src/main/java/com/xiaomi/demo/algo/AlgoTest.java
@@ -12,6 +12,32 @@ import java.util.PriorityQueue;
 @Slf4j
 public class AlgoTest {
 
+    /**
+     * 圆环上有10个点，编号为0~9。从0点出发，每次可以逆时针和顺时针走一步，问走n步回到0点共有多少种走法。
+     * 输入: 2
+     * 输出: 2
+     * 解释：有2种方案。分别是0->1->0和0->9->0
+     */
+    public int circleSteps(int n, int k) {
+        // 圆环中有n个节点，走k步回答原点有几种走法
+        // 走k步走到0的走法=走k-1步走到1的走法 + 走k-1步走到num-1的走法
+        // dp[i][j]表示走i步走到j点的走法种类
+        // dp[i][j] = dp[i-1][(j+1)%len] + dp[i-1][(j-1+len)%len]
+        int[][] dp = new int[k + 1][n];
+        dp[0][0] = 1;
+        for (int i = 1; i <= k; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = dp[i - 1][(j + 1) % n] + dp[i - 1][(j - 1 + n) % n];
+            }
+        }
+        return dp[k][0];
+    }
+
+    @Test
+    public void testCircleSteps() {
+        System.out.println(circleSteps(10, 2));
+    }
+
     @Test
     public void test() {
 //        System.out.println(findMaxForm(new String[]{"10", "0", "1"}, 1, 1));
--- study/src/main/java/com/xiaomi/demo/java/basic/TestFunction.java
+++ study/src/main/java/com/xiaomi/demo/java/basic/TestFunction.java
@@ -8,15 +8,18 @@ import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @Author: liuchiyun
  * @Date: 2023/12/18
+ * https://zhuanlan.zhihu.com/p/531651771
  */
 @Slf4j
 public class TestFunction {
@@ -25,17 +28,17 @@ public class TestFunction {
     public void testFunction1() {
         Function<Integer, Integer> identity = integer -> integer;
 
-        Predicate<Integer> notNull = (num) -> num != null;
+        Predicate<Integer> notNull = Objects::nonNull;
 
-        Consumer<String> printLog = (logStr) -> log.info(logStr);
+        Consumer<String> printLog = log::info;
 
         Supplier<String> stringSupplier = () -> "temp";
     }
 
     @Test
     public void testFunction2() {
-        Map<String, List<User>> collect = Collections.singleton(new User()).stream().collect(Collectors.groupingBy(User::getGroup, Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList)));
-        BigDecimal total = Collections.singleton(BigDecimal.ZERO).stream().reduce(BigDecimal.ZERO, BigDecimal::add);
+        Map<String, List<User>> collect = Stream.of(new User()).collect(Collectors.groupingBy(User::getGroup, Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList)));
+        BigDecimal total = Stream.of(BigDecimal.ZERO).reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
 
