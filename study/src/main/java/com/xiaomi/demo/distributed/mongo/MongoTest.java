5f89310c4ae86519045a5bc5cbb27a2f436d8323
liu <m15879038981@163.com>
2024-06-18 00:08:27 +0800

    add

---
+++ study/src/main/java/com/xiaomi/demo/distributed/mongo/MongoTest.java
@@ -0,0 +1,8 @@
+package com.xiaomi.demo.distributed.mongo;
+
+/**
+ * @Author liuchiyun
+ * @Date 2024/6/17
+ */
+public class MongoTest {
+}
---
+++ study/src/main/java/com/xiaomi/demo/log/Log4j2Test.java
@@ -0,0 +1,8 @@
+package com.xiaomi.demo.log;
+
+/**
+ * @Author liuchiyun
+ * @Date 2024/6/17
+ */
+public class Log4j2Test {
+}
