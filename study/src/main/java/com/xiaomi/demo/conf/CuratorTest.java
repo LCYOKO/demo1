120ffe4a241d5abfcd90da0395bb59ee1595cdbc
liu <m15879038981@163.com>
2024-06-17 23:26:29 +0800

    add

---
+++ study/src/main/java/com/xiaomi/demo/conf/CuratorTest.java
@@ -0,0 +1,22 @@
+package com.xiaomi.demo.conf;
+
+/**
+ * @Author liuchiyun
+ * @Date 2024/6/17
+ */
+public class CuratorTest {
+
+    public void testClient() {
+
+    }
+
+
+    public void testLock() {
+
+    }
+
+
+    public void testLeaderElection() {
+
+    }
+}
---
+++ study/src/main/java/com/xiaomi/demo/conf/ZookeeperTest.java
@@ -0,0 +1,31 @@
+package com.xiaomi.demo.conf;
+
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+
+/**
+ * @Author liuchiyun
+ * @Date 2024/6/17
+ */
+public class ZookeeperTest {
+    private static ZooKeeper zooKeeper;
+
+    static {
+        try {
+            zooKeeper = new ZooKeeper("localhost:2181", 1000, null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void testCreate() {
+
+
+    }
+
+    public void testSetData() {
+        
+    }
+}
