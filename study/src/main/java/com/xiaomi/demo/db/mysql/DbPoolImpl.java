32d48251cdbb6321013013d69218ea4ab67fc4e6
liuchiyun <m15879038981@163.com>
2024-06-20 20:59:25 +0800

    add

--- study/src/main/java/com/xiaomi/demo/db/mysql/DbPoolImpl.java
+++ study/src/main/java/com/xiaomi/demo/db/mysql/DbPoolImpl.java
@@ -1,5 +1,7 @@
 package com.xiaomi.demo.db.mysql;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -11,6 +13,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @Date 2021/1/23 23:20
  * @Version 1.0
  */
+@Slf4j
 public class DbPoolImpl implements DbPool {
 
     /**
@@ -49,8 +52,6 @@ public class DbPoolImpl implements DbPool {
 
     /**
      * 获取一个连接
-     *
-     * @return
      */
     @Override
     public Connection getConnection() {
@@ -59,43 +60,32 @@ public class DbPoolImpl implements DbPool {
         if (connection != null) {
             // 如果有连接，则放入busy池中
             busyConnectPool.offer(connection);
-            System.out.println("获取到连接");
             return connection;
         }
-
-//        synchronized (DbPoolImpl.class) { // 锁--效率低下
-        // idle池中没有连接
-        // 如果idle池中连接未满maxSize，就新建一个连接
+        // idle池中没有连接如果idle池中连接未满maxSize，就新建一个连接
         if (activeSize.get() < maxSize) {
-            // 通过 activeSize.incrementAndGet() <= maxSize 这个判断
-            // 解决 if(activeSize.get() < maxSize) 存在的线程安全问题
+
             if (activeSize.incrementAndGet() <= maxSize) {
-                connection = null;//DbUtil.createConnection();// 创建新连接
+                connection = createConnection();
                 busyConnectPool.offer(connection);
                 return connection;
             }
         }
-//        }
 
-        // 如果空闲池中连接数达到maxSize， 则阻塞等待归还连接
         try {
-            System.out.println("排队等待连接");
-            connection = idleConnectPool.poll(10000, TimeUnit.MILLISECONDS);// 阻塞获取连接，如果10秒内有其他连接释放，
+            connection = idleConnectPool.poll(10000, TimeUnit.MILLISECONDS);
             if (connection == null) {
-                System.out.println("等待超时");
                 throw new RuntimeException("等待连接超时");
             }
-            System.out.println("等待到了一个连接");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        } catch (
+                Exception e) {
+            log.error("get connection failed", e);
         }
         return connection;
     }
 
     /**
      * 释放一个连接
-     *
-     * @param connection
      */
     @Override
     public void releaseConnection(Connection connection) {
@@ -116,9 +106,7 @@ public class DbPoolImpl implements DbPool {
     /**
      * 定时对连接进行健康检查
      * 注意：只能对idle连接池中的连接进行健康检查，
-     * 不可以对busyConnectPool连接池中的连接进行健康检查，因为它正在被客户端使用;
      */
-    //@Scheduled(fixedRate = 60 * 1000)
     public void check() {
         for (int i = 0; i < activeSize.get(); i++) {
             Connection connection = idleConnectPool.poll();
@@ -126,12 +114,16 @@ public class DbPoolImpl implements DbPool {
                 boolean valid = connection.isValid(2000);
                 if (!valid) {
                     // 如果连接不可用，则创建一个新的连接
-                    connection = null;//DbUtil.createConnection();
+                    connection = createConnection();
                 }
-                idleConnectPool.offer(connection);// 放进一个可用的连接
+                idleConnectPool.offer(connection);
             } catch (SQLException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    private Connection createConnection() {
+        return null;
     }
 }
