aa014134182ab949ca4f3134eccf37cbc6608ead
liu <m15879038981@163.com>
2024-06-24 21:59:14 +0800

    add

--- study/src/main/java/com/xiaomi/demo/mq/rocketmq/RocketMqTest.java
+++ study/src/main/java/com/xiaomi/demo/mq/rocketmq/RocketMqTest.java
@@ -1,15 +1,24 @@
 package com.xiaomi.demo.mq.rocketmq;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.*;
+import org.apache.rocketmq.client.exception.MQBrokerException;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
 import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.remoting.exception.RemotingException;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @Author: liuchiyun
@@ -18,16 +27,21 @@ import java.util.Collections;
 @Slf4j
 public class RocketMqTest {
     private DefaultMQProducer publishTemplate;
+    private DefaultMQPushConsumer consumer;
+    private final String TEST_TOPIC = "test";
 
     @Before
-    public void init() {
+    public void init() throws MQClientException {
         publishTemplate = new DefaultMQProducer("testProducer");
-        publishTemplate.setNamesrvAddr("localhost:9876");
+        publishTemplate.setNamesrvAddr("127.0.0.1:9876");
+        publishTemplate.start();
+        consumer = new DefaultMQPushConsumer("CID_JODIE_1");
+        consumer.setNamesrvAddr("127.0.0.1:9876");
     }
 
     @Test
     public void testSimpleSend() {
-        Message msg = new Message("test", ("testSimpleSend ").getBytes());
+        Message msg = new Message(TEST_TOPIC, ("testSimpleSend ").getBytes());
         try {
             SendResult sendResult = publishTemplate.send(msg);
             log.info("result:{}", sendResult);
@@ -38,7 +52,7 @@ public class RocketMqTest {
 
     @Test
     public void testBatchSend() {
-        Message msg = new Message("test", ("testBatchSend").getBytes());
+        Message msg = new Message(TEST_TOPIC, ("testBatchSend").getBytes());
         try {
             SendResult sendResult = publishTemplate.send(Collections.singletonList(msg));
             log.info("result:{}", sendResult);
@@ -78,19 +92,60 @@ public class RocketMqTest {
         }
     }
 
-//    @Test
-//    public void testSendOrderly() throws MQClientException {
-//        Message msg = new Message("test", ("Hello RocketMQ ").getBytes());
-//        try {
-//            SendResult sendResult = publishTemplate.send(msg, Long.parseLong("order"));
-//            System.out.printf("%s%n", sendResult);
-//        } catch (Exception e) {
-//            log.info("send message failed. message:{}", msg, e);
-//        }
-//    }
+    @Test
+    public void testConsumer() throws MQClientException, InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        consumer.subscribe(TEST_TOPIC, "*");
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+        //wrong time format 2017_0422_221800
+        consumer.setConsumeTimestamp("20181109221800");
+        consumer.registerMessageListener((MessageListenerConcurrently) (msgs, context) -> {
+            log.info("mesgs:{}", msgs);
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+        });
+        consumer.start();
+        latch.await();
+    }
 
     @Test
-    public void testConsumer() {
+    public void testSendMessageOrderly() throws MQBrokerException, RemotingException, InterruptedException, MQClientException {
+        String[] tags = new String[]{"TagA", "TagB", "TagC", "TagD", "TagE"};
+        for (int i = 0; i < 10; i++) {
+            int orderId = i % 2;
+            Message msg = new Message("TopicTestjjj", tags[i % tags.length], "KEY" + i, ("Hello RocketMQ " + i).getBytes());
+            SendResult sendResult = publishTemplate.send(msg, (mqs, msg1, arg) -> {
+                Integer id = (Integer) arg;
+                int index = id % mqs.size();
+                return mqs.get(index);
+            }, orderId);
+            log.info("sned result:{}", sendResult);
+        }
+    }
 
+    @Test
+    public void testConsumerOrderly() throws MQClientException, InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("please_rename_unique_group_name_3");
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+        consumer.subscribe(TEST_TOPIC, "TagA || TagC || TagD");
+        consumer.registerMessageListener(new MessageListenerOrderly() {
+            final AtomicLong consumeTimes = new AtomicLong(0);
+
+            @Override
+            public ConsumeOrderlyStatus consumeMessage(List<MessageExt> msgs, ConsumeOrderlyContext context) {
+                context.setAutoCommit(true);
+                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
+                this.consumeTimes.incrementAndGet();
+                if ((this.consumeTimes.get() % 2) == 0) {
+                    return ConsumeOrderlyStatus.SUCCESS;
+                } else if ((this.consumeTimes.get() % 5) == 0) {
+                    context.setSuspendCurrentQueueTimeMillis(3000);
+                    return ConsumeOrderlyStatus.SUSPEND_CURRENT_QUEUE_A_MOMENT;
+                }
+                return ConsumeOrderlyStatus.SUCCESS;
+            }
+        });
+        consumer.start();
+        latch.await();
     }
 }
