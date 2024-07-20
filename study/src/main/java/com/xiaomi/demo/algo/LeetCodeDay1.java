32f3706af28603e631a0dedb6eff69b567297f15
liuchiyun <m15879038981@163.com>
2024-07-20 17:55:00 +0800

    add

---
+++ study/src/main/java/com/xiaomi/demo/algo/LeetCodeDay1.java
@@ -0,0 +1,20 @@
+package com.xiaomi.demo.algo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * @Author: liuchiyun
+ * @Date: 2024/7/20
+ */
+@Slf4j
+public class LeetCodeDay1 {
+
+    @Test
+    public void test() {
+    }
+
+}
